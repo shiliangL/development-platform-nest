88521f061f8d6bb6dbc63cfb88cdaab6ff91fa49
shiliangL <1462999406@qq.com>
2023-03-01 14:19:42 +0800

    2023年3月1日++

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,9 @@ async function bootstrap() {
     .setDescription('系统接口文档') // 文档介绍
     .setVersion('1.0.0') // 文档版本
     .build();
-  // 为了创建完整的文档（具有定义的HTTP路由），我们使用类的createDocument()方法SwaggerModule。此方法带有两个参数，分别是应用程序实例和基本Swagger选项。
+  // 为了创建完整的文档（具有定义的HTTP路由），
+  // 我们使用类的createDocument()方法SwaggerModule。
+  // 此方法带有两个参数，分别是应用程序实例和基本Swagger选项。
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/api', app, document);
   await app.listen(3000);
